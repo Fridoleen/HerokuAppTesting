a028b9a56c308276a73d7f06f56f124fecb5817a
Mikoliuk Dmitry <fridoleen@gmail.com>
2021-08-11 13:45:43 +0300

    Included Nuget test package, just need to configure allure

--- HerokuAppTesting/LoginTest.cs
+++ HerokuAppTesting/LoginTest.cs
@@ -1,6 +1,7 @@
 ﻿using Allure.Commons;
 using NUnit.Allure.Attributes;
 using NUnit.Allure.Core;
+using NUnit.Allure.Steps;
 using NUnit.Framework;
 using System;
 
@@ -21,12 +22,20 @@ namespace HerokuAppTesting
         [AllureStep("Step One")]
         public void GreetUser()
         {
-            Console.WriteLine("What's your bidding, master?");
+            Console.WriteLine("What's your bidding, master? There must be a screenshot of");
         }
 
+        [Test]
+        [AllureStep("Test LigIn")]
         public void TestCaseOne()
         {
+            DriverHelper.InitializeChrome("https://the-internet.herokuapp.com/login");
+            var loginPage = new LoginPage(DriverHelper.Driver);
+            loginPage.loginUser("tomsmith", "SuperSecretPassword!");
 
+            var secureAreaPage = new SecureAreaPage(DriverHelper.Driver);
+            string text = "You logged into a secure area!";
+            Assert.AreEqual(secureAreaPage.getMessageText().Contains(text), true);
         }
     }
 }
